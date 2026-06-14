a1ca2d99a586a12da1153365fa03bca71199fe4e
nhrafin98405 <nhrafin98405@.com>
2026-06-15 01:10:30 +0600

    add

---
+++ laravel12_project4/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\FirstControllers;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/',[FirstControllers::class,'home']);
+Route::get('/about',[FirstControllers::class,'about']);
+Route::get('/contact',[FirstControllers::class,'contact']);
+
+?>
